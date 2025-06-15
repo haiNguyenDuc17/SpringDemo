dfe8156a05eb207daae09315c8e4d73059b44f48
APAC\PMA4HC <Anh.PhamDuc@vn.bosch.com>
2025-06-15 10:09:54 +0700

    Add ingress

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 git pull
-#kubectl delete -f manifests/deployment.yaml
+kubectl delete -f manifests/deployment.yaml
 kubectl apply -f manifests/deployment.yaml
+kubectl delete -f manifests/ingress.yaml
 kubectl apply -f manifests/ingress.yaml
